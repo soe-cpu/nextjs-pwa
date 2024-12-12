5bac312bb36dd80e41d41c66c66641674a37e26a
soe-cpu <soelinaung709@gmail.com>
2024-12-12 12:09:44 +0630

    manifest changed

--- app/manifest.ts
+++ app/manifest.ts
@@ -5,21 +5,25 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Next.js PWA",
     short_name: "NextPWA",
     description: "A Progressive Web App built with Next.js",
-    start_url: "/",
+    start_url: "http://localhost:3000/",
     display: "standalone",
+    icons: [
+      {
+        purpose: "maskable",
+        sizes: "512x512",
+        src: "icon512_maskable.png",
+        type: "image/png",
+      },
+      {
+        purpose: "any",
+        sizes: "512x512",
+        src: "icon512_rounded.png",
+        type: "image/png",
+      },
+    ],
+    dir: "auto",
+    lang: "en-GB",
     background_color: "#ffffff",
     theme_color: "#000000",
-    // icons: [
-    //   {
-    //     src: "/icon-192x192.png",
-    //     sizes: "192x192",
-    //     type: "image/png",
-    //   },
-    //   {
-    //     src: "/icon-512x512.png",
-    //     sizes: "512x512",
-    //     type: "image/png",
-    //   },
-    // ],
   };
 }
